5a310fa204c6ac364ccfd14f2b4ae269a3e4c6df
Tao Jiang <hijiangtao@gmail.com>
2020-05-14 20:17:23 +0800

    ADD: @fudao/helloo init

---
+++ projects/hello/src/lib/hello.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { HelloComponent } from './hello.component';
+
+
+
+@NgModule({
+  declarations: [HelloComponent],
+  imports: [
+  ],
+  exports: [HelloComponent]
+})
+export class HelloModule { }
---
+++ projects/hello/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of hello
+ */
+
+export * from './lib/hello.service';
+export * from './lib/hello.component';
+export * from './lib/hello.module';
